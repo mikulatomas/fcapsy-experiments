f41a091cf7d6934b2bdfc96c764060415e3e5764
Tomáš Mikula <mail@tomasmikula.cz>
2022-01-21 14:26:12 +0100

    v0.2.3 (#5)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fcapsy-experiments",
-    version="0.2.2",
+    version="0.2.3",
     author="Tomáš Mikula",
     author_email="mail@tomasmikula.cz",
     description="Package of experiments for fcapsy library.",
